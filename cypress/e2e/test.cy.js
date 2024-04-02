24e7ae6bc9bd3cffee7ee3f521f8d4ce9f8150b8
Iulian Gioada <iulian.gioada@thinslices.com>
2024-04-02 23:30:29 +0300

    Add log

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,8 +1,7 @@
 describe("cypress-diff-testing", () => {
   const data = [];
   before(() => {
-    console.log(process.env.CYPRESS_PREV_RUN_JSON);
-
+    cy.task("log", JSON.stringify(process.env, null, 2));
     if (process.env.CYPRESS_PREV_RUN_JSON) {
       cy.fixture(process.env.CYPRESS_PREV_RUN_JSON).then((d) => {
         data = JSON.parse(d);
