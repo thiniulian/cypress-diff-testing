08ad92c8318c7322f514ac6c4c30b231c74f6d35
Iulian Gioada <iulian.gioada@thinslices.com>
2024-04-02 23:41:47 +0300

    use Cypress.env

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,9 +1,8 @@
 describe("cypress-diff-testing", () => {
   const data = [];
   before(() => {
-    cy.task("log", JSON.stringify(process.env, null, 2));
-    if (process.env.CYPRESS_PREV_RUN_JSON) {
-      cy.fixture(process.env.CYPRESS_PREV_RUN_JSON).then((d) => {
+    if (Cypress.env("PREV_RUN_JSON")) {
+      cy.fixture(Cypress.env("PREV_RUN_JSON")).then((d) => {
         data = JSON.parse(d);
       });
     }
@@ -11,6 +10,6 @@ describe("cypress-diff-testing", () => {
 
   it("produces a new file", () => {
     const newList = ["https://google.com"];
-    cy.writeFile(process.env.CYPRESS_PREV_RUN_JSON, JSON.stringify(newList));
+    cy.writeFile(Cypress.env("PREV_RUN_JSON"), JSON.stringify(newList));
   });
 });
